92f02ecfbbbe47bda218e27a0428a5267e21a080
Fuzzles <fuzzmyrim@gmail.com>
2026-06-03 01:11:51 -0700

    clib_util has

--- src/System/Utils.h
+++ src/System/Utils.h
@@ -2,20 +2,6 @@
 
 namespace Utils
 {
-	inline bool IContains(std::string_view haystack, std::string_view needle)
-	{
-		if (needle.empty())
-			return true;
-		auto it = std::search(
-			haystack.begin(), haystack.end(),
-			needle.begin(), needle.end(),
-			[](char ch1, char ch2) {
-				return std::toupper(static_cast<unsigned char>(ch1)) ==
-			           std::toupper(static_cast<unsigned char>(ch2));
-			});
-		return it != haystack.end();
-	}
-
 	inline void SanitizePath(char* dest, const char* source, std::size_t destSize)
 	{
 		if (!dest || !source || destSize == 0)
