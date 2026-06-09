56a41e4a5af4f7f9dffd70c2414be53d1235f43a
Fuzzles <fuzzmyrim@gmail.com>
2026-06-08 20:22:01 -0700

    style editor novanity/positioning

--- src/FUCK-Styles.h
+++ src/FUCK-Styles.h
@@ -10,15 +10,15 @@ public:
 	bool        IsOpen() const override { return _isOpen; }
 	void        SetOpen(bool a_open) override;
 
-	FUCK::WindowFlags GetFlags() const override { return FUCK::WindowFlags::kExtendBorder | FUCK::WindowFlags::kCloseOnEsc; }
+	FUCK::WindowFlags GetFlags() const override { return FUCK::WindowFlags::kExtendBorder | FUCK::WindowFlags::kCloseOnEsc | FUCK::WindowFlags::kBlockVanity; }
 
 	ImVec2 GetDefaultSize() const override
 	{
-		return FUCK::UIScale(450.0f, 600.0f);
+		return FUCK::UIScale(465.0f, 510.0f);
 	}
 	ImVec2 GetDefaultPos() const override
 	{
-		return FUCK::Scale(1050.0f, 450.0f);
+		return FUCK::Scale(1360.0f, 460.0f);
 	}
 
 	bool _isOpen = false;
